e768d0532b9bb9d4e11c8a67d6c2a3f139ec49be
denis <denniiskim@gmail.com>
2023-10-25 04:33:47 +0300

    comppleted assignment

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(data))
+  .catch(error => console.error(error));
 }
 
 function renderBooks(books) {
